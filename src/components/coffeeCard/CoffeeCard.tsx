971584e904e711477e49dc6bd7a76350ff2ca9f7
Ilya Shostak <ishostak@lenovo.com>
2023-04-23 22:25:50 +0200

    added route for single product card. Missed types for react component was added

---
+++ src/components/coffeeCard/CoffeeCard.tsx
@@ -0,0 +1,14 @@
+import coffee from "../../hooks/http.hook";
+import './coffeeCard.scss';
+
+const CoffeeCard: React.FC<coffee> = ({ image, title, price }: coffee) => {
+    return (
+        <div className='coffee__card'>
+            <img src={image} alt="solimo" className='coffee__logo' />
+            <p className='coffee__name'>{ title }</p>
+            <p className='coffee__price'>{ price }</p>
+        </div>
+    )
+}
+
+export default CoffeeCard;
--- src/components/singleProduct/SingleProduct.tsx
+++ src/components/singleProduct/SingleProduct.tsx
@@ -1,12 +1,17 @@
+import coffee from '../../hooks/http.hook';
 import black from '../../resources/img/coffe-logo-black.png';
-import aromistico from '../../resources/img/aromistico-big.jpg';
 import './singleProduct.scss';
+import { useLocation } from 'react-router';
 
 
-const SingleProduct = () => {
+const SingleProduct: React.FC = () => {
+
+    const { state } = useLocation();
+    const { image, country, description, price } = state as coffee;
+
     return (                       
       <div className='single'>
-            <img src={aromistico} alt="beans" className='single__img'/>
+            <img src={image} alt="beans" className='single__img'/>
             <div className='single__wrapper'>
                 <h2 className='single__title'>About it</h2>
                 <div className='inner'>
@@ -15,9 +20,9 @@ const SingleProduct = () => {
                     <hr className='inner__line'/>
                 </div>
                 <div className='single__container'>
-                    <p className='single__country'>Country: Brasil</p>
-                    <p className='single__descr'>Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                    <p className='single__price'>Price: 16.99$</p>
+                    <p className='single__country'>{ `Country: ${country}` }</p>
+                    <p className='single__descr'>{ `Description: ${description}` }</p>
+                    <p className='single__price'>{ `Price: ${price}` }</p>
                 </div>
             </div>
         </div>
